109c641809ffad71de912cf7865703da03793bb3
Hieu Tran <hieutran.fu@gmail.com>
2021-06-16 17:57:47 +0700

    feat: add spacing props

--- packages/ui/src/templates/SidebarLayout/SidebarLayout.tsx
+++ packages/ui/src/templates/SidebarLayout/SidebarLayout.tsx
@@ -5,7 +5,7 @@ import {
   DropdownLink,
 } from '../../molecules/Dropdown';
 import { Image } from '../../atoms/Image';
-import { Nav } from '../../molecules/Nav';
+import { Nav, NavLink } from '../../molecules/Nav';
 
 export const SidebarLayout: React.FunctionComponent = () => {
   return (
@@ -200,10 +200,7 @@ export const SidebarLayout: React.FunctionComponent = () => {
             </div>
             <div className="flex-1 flex flex-col overflow-y-auto">
               <Nav className="flex-1  bg-gray-800 ">
-                <a
-                  href="#"
-                  className="bg-gray-900 text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                >
+                <NavLink href="#" active>
                   <svg
                     className="text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -220,12 +217,9 @@ export const SidebarLayout: React.FunctionComponent = () => {
                     />
                   </svg>
                   Dashboard
-                </a>
+                </NavLink>
 
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                >
+                <NavLink href="#">
                   <svg
                     className="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -242,95 +236,7 @@ export const SidebarLayout: React.FunctionComponent = () => {
                     />
                   </svg>
                   Team
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                >
-                  <svg
-                    className="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-                    />
-                  </svg>
-                  Projects
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                >
-                  <svg
-                    className="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                    />
-                  </svg>
-                  Calendar
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                >
-                  <svg
-                    className="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                    />
-                  </svg>
-                  Documents
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                >
-                  <svg
-                    className="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                    />
-                  </svg>
-                  Reports
-                </a>
+                </NavLink>
               </Nav>
             </div>
           </div>
---
+++ packages/ui/src/types/system.ts
@@ -0,0 +1,31 @@
+export interface SpaceProps {
+  m?: number | number[];
+
+  mt?: number | number[];
+
+  mr?: number | number[];
+
+  mb?: number | number[];
+
+  ml?: number | number[];
+
+  mx?: number | number[];
+
+  my?: number | number[];
+
+  p?: number | number[];
+
+  pt?: number | number[];
+
+  pr?: number | number[];
+
+  pb?: number | number[];
+
+  pl?: number | number[];
+
+  px?: number | number[];
+
+  py?: number | number[];
+}
+
+export interface StyleProps extends SpaceProps {}
